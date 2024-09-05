23ce3ba536129d3368e0d199fe3fcaa99f90a167
Baejita <homokoro@gmail.com>
2024-09-05 11:12:20 +0700

    createSnippet

---
+++ app/snippets/new/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <form>
+        <h3 className='font-bold m-3'>Crate Snippet</h3>
+        <div className=' flex '></div>
+
+    </form>
+  )
+}
+
+export default page
