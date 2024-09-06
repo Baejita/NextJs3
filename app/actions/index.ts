d0b352096d7be7080edcbe5a1962b7cd3a0fb2fe
Baejita <homokoro@gmail.com>
2024-09-06 22:33:14 +0700

    try catch

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -17,3 +17,55 @@ export async function deleteSnippet (id:number) {
     })
     redirect('/')
 }
+
+export async function createSnippet(formState: { message: string }, formData: FormData) {
+    try {
+    // Retrieve the title and code from formData
+    const title = formData.get('title');
+    const code = formData.get('code');
+
+    // Check if title and code exist and are strings
+    if (typeof title !== 'string' || typeof code !== 'string') {
+        return {
+            message: "Invalid data provided"
+        };
+    }
+
+    // Validate the title length
+    if (title.length < 3) {
+        return {
+            message: "Title must be longer"
+        };
+    }
+
+    if(typeof code !== 'string' || code.length < 10) {
+        return {
+            message: 'Code must be longer'
+        }
+    }
+
+    // Save the new snippet to the database
+    
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        });
+    }catch (err:unknown) {
+        if(err instanceof Error) {
+            return {
+                message: err.message
+            }
+        }else{
+            return {
+                message: 'Somthing went wrong...'
+            }
+        }
+
+    }
+
+    // Redirect the user to the list page
+    redirect('/');
+ 
+}
--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,30 +1,12 @@
-import { db } from '@/app/db/inde'
-import { redirect } from 'next/navigation';
-import React from 'react'
+'use client'
 
+import { useFormState } from "react-dom"
+import * as actions from '../../actions'
 function snippetCreatePage() {
-    async function createSnippet(formdata:FormData) {
-        //จำเป็นเรียกใช้ server actions ต้องใช้เยอะๆเพราะจะทำให้แอปรวดเร็วขึ้นได้
-        "use server"
-        // ตรวจสอบว่า ผู้ใช้ กรอกข้อมูล ถูกต้อง
-        const title = formdata.get('title' as string);
-        const code = formdata.get('code' as string)
-        // สร้างข้อมูลใหม่ในฐานข้อมูล
-        const snippet = await db.snippet.create({
-            data: {
-                title ,
-                code
-            }
-        })
-
-        console.log(snippet);
-        
-        // นำทางผู้ใช้ไปยังหน้าจอ list
-        redirect('/')
-        
-    }
+  const [formState, action] = useFormState(actions.createSnippet, {message: ''})
+    
   return (
-    <form action={createSnippet}>
+    <form action={action}>
         <h3 className='font-bold m-3'>Crate Snippet</h3>
         <div className=' flex flex-col gap-4'>
             <div className='flex gap-4'>
@@ -36,6 +18,9 @@ function snippetCreatePage() {
                 <label className="w-12" htmlFor='code'>Code</label>
                 <textarea name='code' className='border rounded p-2 w-full' id='code'/>
             </div>
+        <div>
+            {formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null}
+        </div>
 
             <button type="submit" className=' rounded bg-blue-200 p-2'>Create</button>
         </div>
